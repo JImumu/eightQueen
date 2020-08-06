d905d961e7de98cd187902a2c8eb5dd64f9cc976
JImumu <jimumu8@163.com>
2020-08-06 16:09:09 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const check = (x, y, checkerboard) => {
+    let safe = true
+    for (let i = 0; i < checkerboard.length; i++) {
+      if (checkerboard[i].x === x || checkerboard[i].y === y) {
+          safe = false
+          break
+      } else if (Math.abs(checkerboard[i].x - x) === Math.abs(checkerboard[i].y - y)) {
+          safe = false
+          break
+      }
+    }
+    return safe
+}
+
+const getAllMethods = n => {
+    if (n < 4) {
+        throw new Error('棋盘宽度小于4，无解')
+    }
+    let res = []
+
+    const setCheckerboard = (currentCol, end, checkerboard = []) => {
+        for (let i = 0; i < end; i++) {
+            if (check(currentCol, i, checkerboard)) {
+                let checkerboardNext = [...checkerboard]
+                checkerboardNext.push({x: currentCol, y: i})
+                if (currentCol === end - 1) {
+                    res.push(checkerboardNext)
+                } else {
+                    setCheckerboard(currentCol + 1, end, checkerboardNext)
+                }
+            }
+        }
+    }
+    setCheckerboard(0, n)
+    return res
+}
+getAllMethods(8)
